b8ab2d4e55ce41a649b7bea5f8b090043518c70e
41343101 <41343101@nfu.edu.tw>
2025-10-13 13:38:48 +0800

    Create 1-2.cpp

---
+++ homework1/src/1-2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int ack(int m, int n) {
+    const int MAX = 10000;
+    int M[MAX];
+    int top = 0;
+    M[top++] = m;
+
+    while (top > 0) {
+        m = M[--top];
+
+        if (m == 0)  n += 1;
+        else if (n == 0) {
+            M[top++] = m - 1;
+            n = 1;
+        }
+        else {
+            M[top++] = m - 1;
+            M[top++] = m;
+            n -= 1;
+        }
+    }
+    return n;
+}
+int main() {
+    int m, n;
+    cin >> m >> n;
+    cout << ack(m, n) << endl;
+    return 0;
+}
